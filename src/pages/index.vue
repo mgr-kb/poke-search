5542d48ca2b9047cf42bb28dd0312a6a0676b13e
m.g <koumgrars13@gmail.com>
2023-02-04 21:53:41 +0900

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Nuxt3 ESLint Prettier</h1>
+    <div>
+      count: {{ count }}
+      <div>
+        <button @click="plus">plus</button>
+        <button @click="minus">minus</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+const count = ref(0);
+const plus = () => count.value++;
+const minus = () => count.value--;
+</script>
